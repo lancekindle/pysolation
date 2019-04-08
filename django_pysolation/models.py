7a3878f872e8fc94670e4a29c308e2708b51b2d9
Lance Kindle <lance.kindle@gmail.com>
2019-04-07 23:35:36 -0700

    display board with links
    127.0.0.1:8000/game shows initial board layout. Every-time the page is
    reloaded the board and players are all new.

--- django_pysolation/models.py
+++ django_pysolation/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    w = models.IntegerField()
+    h = models.IntegerField()
 
-class Board:
-    pass
+class Tile(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    ID = models.CharField(max_length=15)
+    visible = models.BooleanField()
+    solid = models.BooleanField()
+
+class Player(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    ID = models.CharField(max_length=15)  # player_1, player_2, etc.
+    x = models.IntegerField()
+    y = models.IntegerField()
+    disabled = models.BooleanField()    # True if player is unable to move
+    active = models.BooleanField()  # True if it's players turn
--- django_pysolation/views.py
+++ django_pysolation/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from HtmlBoard import HtmlGameBoard, HtmlGame, HtmlPlayer, HtmlTile
+from . import models
+
+def load_game_from_django(django_game):
+    django_board = django_game.board
+    # order by 'id' NOT 'ID' aka order by creation order
+    tiles = models.Tile.objects.filter(board__eq=django_board).order_by('id')
+    players = models.Player.objects.filter(board__eq=django_board).order_by('id')
+    game = HtmlGame()
+    gb = HtmlGameBoard()
+    game.board = gb
+    gb.setup()
+    gb.players.clear()
+    for p in players:
+        player = HtmlPlayer.create(p.x, p.y)
+        player.active = p.active
+        player.disabled = p.disabled
+        player.ID = p.ID
+        gb.players.append(player)
+    gb.fill_board(django_board.w, django_board.h)
+    # replace tiles with tiles from django
+    for (x, y, _), tile in zip(gb, tiles):
+        gb[x, y] = tile
+    return game
+
+def save_game_to_django(game):
+    # create database entries for board, tiles, and players
+    board = game.board
+    django_board = models.Board(h=board.h, w=board.w)
+    for x, y, tile in board:
+        models.Tile(board=django_board, x=x, y=y,
+             ID=tile.ID, visible=tile.visible, solid=tile.solid)
+    for player in board.players:
+        models.Player(board=django_board, ID=player.ID, x=player.x,
+               y=player.y, disabled=player.disabled, active=player.active)
 
 # Create your views here.
 def index(request):
-    return HttpResponse("index / landing page")
+    game = HtmlGame()
+    game.setup()  # sets up common 9x9 board with 2 players
+    save_game_to_django(game)
+    game.prep_links()
+    # now we display the main board to the user.... 
+    context = {
+              "game": game,
+              "board": game.board,
+    }
+    return render(request, 'django_pysolation/index.html', context=context)
+
+def move_player(request):
+    request.args
